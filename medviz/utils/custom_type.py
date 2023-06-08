3f1349bd691c5f958ffea9b8c3df4b8012c54305
Mohsen Hariri <mhariri68@gmail.com>
2023-06-07 20:35:16 -0400

    path custom

--- medviz/utils/custom_type.py
+++ medviz/utils/custom_type.py
@@ -1,5 +1,17 @@
 from pathlib import Path
-from typing import Iterable, Union
+from typing import Iterable, List, Union
 
-PathTypeIter = Union[str, Path, Iterable[str], Iterable[Path]]
 PathType = Union[str, Path]
+# example:
+# path: PathType = Path("/storage/git/mohsen/medviz/dataset/1-1.nii")
+# path: PathType = "/storage/git/mohsen/medviz/dataset/1-1.nii"
+PathTypeLst = Union[List[Path], List[str]]
+# example:
+# paths: PathTypeLst = [Path("/storage/git/mohsen/medviz/dataset/1-1.nii"), Path("/storage/git/mohsen/medviz/dataset/1-1.nii")]
+# paths: PathTypeLst = ["/storage/git/mohsen/medviz/dataset/1-1.nii", "/storage/git/mohsen/medviz/dataset/1-1.nii"]
+PathTypeIter = Union[str, Path, Iterable[str], Iterable[Path]]
+# example:
+# paths: PathTypeIter = Path("/storage/git/mohsen/medviz/dataset/1-1.nii")
+# paths: PathTypeIter = "/storage/git/mohsen/medviz/dataset/1-1.nii"
+# paths: PathTypeIter = ["/storage/git/mohsen/medviz/dataset/1-1.nii", "/storage/git/mohsen/medviz/dataset/1-1.nii"]
+# paths: PathTypeIter = [Path("/storage/git/mohsen/medviz/dataset/1-1.nii"), Path("/storage/git/mohsen/medviz/dataset/1-1.nii")]
