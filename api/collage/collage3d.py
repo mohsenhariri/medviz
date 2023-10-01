1885406d7f10ea3e116b12b1d0478e638a6f4dcc
Mohsen Hariri <mhariri68@gmail.com>
2023-10-01 13:15:03 -0400

    col3d usage

---
+++ api/collage/collage3d.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import medviz as viz
+
+image_path = Path(
+    "/storage/sync/git/mohsen/medviz/test_local/dataset/pc/349090_CTE_AX_Res.nii"
+)
+mask_path = Path(
+    "/storage/sync/git/mohsen/medviz/test_local/dataset/pc/349090_CTE_AX-ng-label_Res.nii"
+)
+
+# read image and mask without normalization
+image, mask = viz.read_image_mask(image=image_path, mask=mask_path)
+
+# read image and mask with normalization to [min, max] for the image
+# image, mask = viz.read_image_mask(image=image_path, mask=mask_path, norm=True)
+
+# read image and mask with normalization to [mean, std] for the image
+# image, mask = viz.read_image_mask(image=image_path, mask=mask_path, norm=(0, 1))
+
+# viz.feats.collage3d(
+#     image, mask, window_sizes=[3, 5, 7, 9, 11], save_path="col3_cd", out_name="whole"
+# )
+
+viz.feats.collage3d(
+    image,
+    mask,
+    window_sizes=[3, 5, 7, 9, 11],
+    save_path="col3_cd",
+    out_name="pad11",
+    padding=True,
+)
+
+
+viz.feats.collage3d(
+    image,
+    mask,
+    window_sizes=[3, 5, 7, 9, 11],
+    save_path="col3d_cd",
+    out_name="pad20",
+    padding=20,
+)
